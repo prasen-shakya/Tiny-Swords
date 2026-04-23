54f7c594f4c22c8af613330fd731edf89d72f3aa
Prasen Shakya <68079724+prasen-shakya@users.noreply.github.com>
2026-04-23 11:06:03 -0700

    Add shaman projectile attack

---
+++ entities/enemies/shaman/projectile.gd
@@ -0,0 +1,72 @@
+extends Node2D
+
+@export var speed := 320.0
+@export var fade_duration := 0.2
+@export var max_travel_distance := 400.0
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var hurtbox_area: Area2D = $HurtboxArea
+@onready var hurtbox_shape: CollisionShape2D = $HurtboxArea/HurtboxShape
+
+var fly_direction := Vector2.RIGHT
+var spawn_position := Vector2.ZERO
+var damage: int = 0
+var is_exploding := false
+var is_fading_out := false
+
+func _ready() -> void:
+	spawn_position = global_position
+
+func fly(direction: Vector2) -> void:
+	if direction.is_zero_approx():
+		return
+
+	spawn_position = global_position
+	fly_direction = direction.normalized()
+
+func _physics_process(delta: float) -> void:
+	if is_exploding or is_fading_out:
+		return
+
+	global_position += fly_direction * speed * delta
+
+	if global_position.distance_squared_to(spawn_position) >= max_travel_distance * max_travel_distance:
+		fade_away()
+
+func explode() -> void:
+	if is_exploding or is_fading_out:
+		return
+
+	is_exploding = true
+	_disable_hitbox()
+	animated_sprite.play("explosion")
+
+func fade_away() -> void:
+	if is_exploding or is_fading_out:
+		return
+
+	is_fading_out = true
+	_disable_hitbox()
+
+	var fade_tween := create_tween()
+	fade_tween.tween_property(animated_sprite, "modulate:a", 0.0, fade_duration)
+	fade_tween.finished.connect(queue_free)
+
+func _on_hurtbox_area_area_entered(area: Area2D) -> void:
+	if area.owner == self:
+		return
+	
+	area.owner.take_damage(damage)
+	explode()
+
+func _on_hurtbox_area_body_entered(_body: Node2D) -> void:
+	explode()
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite.animation == "explosion":
+		queue_free()
+
+func _disable_hitbox() -> void:
+	hurtbox_area.monitoring = false
+	hurtbox_area.monitorable = false
+	hurtbox_shape.set_deferred("disabled", true)
--- entities/enemies/shaman/shaman.gd
+++ entities/enemies/shaman/shaman.gd
@@ -1,1 +1,30 @@
 extends "res://scripts/characters/enemy.gd"
+
+@export var projectile_effect: PackedScene
+@onready var projectile_spawn: Marker2D = $ProjectileSpawn
+@onready var base_projectile_spawn := projectile_spawn.position
+
+func spawn_projectile() -> void:
+	var projectile_scene := projectile_effect.instantiate()
+	projectile_scene.global_position = projectile_spawn.global_position
+	projectile_scene.max_travel_distance = _get_projectile_max_travel_distance()
+	projectile_scene.fly((player.global_position - projectile_spawn.global_position).normalized())
+	projectile_scene.damage = attack_damage
+	get_parent().add_child(projectile_scene)
+
+func _update_facing(direction_x: float) -> void:
+	if is_zero_approx(direction_x):
+		return
+
+	super._update_facing(direction_x)
+	
+	var facing_left := direction_x < 0
+	projectile_spawn.position.x = abs(base_projectile_spawn.x) if facing_left else -abs(base_projectile_spawn.x)
+
+func _get_projectile_max_travel_distance() -> float:
+	var range_shape := attack_range_shape.shape as CircleShape2D
+
+	if range_shape:
+		return range_shape.radius + 50
+
+	return 0.0
